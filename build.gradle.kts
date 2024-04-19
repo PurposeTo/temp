9faf07713a7508f66deeb2d558cdc18013f49140
v.v.vasilev <v.v.vasilev@tinkoff.ru>
2024-04-19 19:07:41 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+   java
+}
+
+group = "org.temp.net"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
