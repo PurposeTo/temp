7bc57c49f3a87629830e92b3fd59b618ab0b1b40
v.v.vasilev <v.v.vasilev@tinkoff.ru>
2024-04-25 12:46:53 +0500

    Use StringTemplate

--- src/main/java/org/temp/net/Main.java
+++ src/main/java/org/temp/net/Main.java
@@ -1,8 +1,11 @@
 package org.temp.net;
 
+import static java.lang.StringTemplate.STR;
+
 public class Main {
+    private static final String NAME = "world";
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(STR."Hello \{NAME}!");
     }
 }
